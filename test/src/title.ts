5362d4dc8f230fe88e442cb9866b99ab9c187144
proudbird <v6825403@hotmail.com>
2022-01-08 10:55:02 +0100

    adopting

--- test/src/title.ts
+++ test/src/title.ts
@@ -1,11 +1,20 @@
 export default function title(set: {inputs: any[], etalons: any[]}, index: number, text: string): string {
   let input  = set.inputs[index];
   let etalon = set.etalons[index];
-  if(input === '') {
-    input = 'empty string';
-  }
-  if(etalon === '') {
-    etalon = 'empty string';
-  }
+  input  = adopt(input);
+  etalon = adopt(etalon);
+
   return `${input} ${text} ${etalon}`;
+}
+
+function adopt(value: any): string {
+  if(value === '') {
+    value = 'empty string';
+  }
+
+  if(typeof value === 'string') {
+    value = `'${value}'`;
+  }
+
+  return value;
 }
