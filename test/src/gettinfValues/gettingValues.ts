fb0eb0ee889a39a874fc8c04d58d7574b69f69fa
proudbird <v6825403@hotmail.com>
2022-01-08 10:55:27 +0100

    boolean and date test added

--- test/src/gettinfValues/gettingValues.ts
+++ test/src/gettinfValues/gettingValues.ts
@@ -1,9 +1,13 @@
 import gettingString from './string';
 import gettingNumbers from './number';
+import gettingBoolean from './boolean';
+import gettingDate from './date';
 
 export default function() {
   describe('Value transformation', () => {
     gettingString();
     gettingNumbers();
+    gettingBoolean();
+    gettingDate();
   });
 }
